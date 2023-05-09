7ab92799d7d557678ca8985ddffd9ab980291da0
dev <Bichk_di78@student.itstep.org>
2023-05-09 20:34:49 +0300

    1

--- main.py
+++ main.py
@@ -1,16 +1,54 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class School:
+    def __init__(self , name , students):
+        self.name = name
+        self.students = students
+        self.teachers = []                          #список студентів
+    def admit_student(self , student): #додавання студентів
+        self.students.append(student)
+        print(f"{student.name} був доданий до школи {self.name}")  #дописати
+    def expel_student(self , student): # видалення студента
+        expelled_student = next(filter(lambda s : s.name == student.name and s.grade == student.grade , self.students) , None)
+        if expelled_student is not None:
+           self.students.remove(expelled_student)
+           print(f"{expelled_student.name} був видалений с {self.name}")
+        else:
+           print(f"{student.name} не був знайдений  {self.name}")
+    def add_teacher(self , teacher):
+        self.teachers.append(teacher)
+        print(f"{teacher.name} був доданий до школи {self.name}")
+
+class Teacher:
+    def __init__(self , name , subjekt , clas):
+        self.name = name
+        self.subjekt = subjekt
+        self.clas = clas
+    
+class Student:
+    def __init__(self , name , grade):
+        self.name = name
+        self.grade = grade
+    def promote(self):
+        self.grade += 1
+        print(f"{self.name} був підвіщений {self.grade}")
+    def demote(self):
+        self.grade -= 1
+        print(f"{self.name} був понижений {self.grade}")
+    def __str__(self):
+        return f"{self.name} - Ранг {self.grade}"   #додали метод виводу
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+lisa = Student("Alisa" , 6)
+masha = Student("Masha" , 2)
+dasha = Student("Dasha" , 50)
+dima = Student("Dmutro" , 23)
+gleb = Student("Gleb" , 100)
+my_school = School("ItStep" , [lisa , masha , dasha , dima , gleb])
+print(f"Початкові студенти")
+for student in my_school.students:
+    print(student)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("Оновлення")
+my_school.admit_student(Student("Bogdan" , 3))
+my_school.expel_student(Student("Alisa" , 6))
